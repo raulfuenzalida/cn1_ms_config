a73a144ec41a36859a59b52ecab9f94276921257
Raúl Fuenzalida <rau.fuenzalida@duocuc.cl>
2026-09-11 08:52:11 -0300

    Add new allowed origin for CORS configuration

--- src/main/java/duoc/cn1/ms_config/security/SecurityConfig.java
+++ src/main/java/duoc/cn1/ms_config/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration cfg = new CorsConfiguration();
-		cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4200"));
+		cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4200", "https://raulfuenzalida.github.io"));
 		cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		cfg.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin"));
 		cfg.setAllowCredentials(true);
